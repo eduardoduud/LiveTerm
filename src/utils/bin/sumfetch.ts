31364b83094bef0fe9bf4d5da2b3c042e6865faf
EduardoDomingues <eeadomingues@gmail.com>
2022-10-27 17:08:19 -0300

    modified:   src/utils/bin/sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -23,21 +23,21 @@ const sumfetch = async (args: string[]): Promise<string> => {
 `;
   } else {
     return `
-           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
-        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
-      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
-    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
-▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github Repository</a></u>
-▐▓                                 ▐▓       -----------
-▐▓           > D U U D             ▐▓        CONTACT 
-▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             
-        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
-            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  PROFILE
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              I have 10 years of experience in IT. Started in mid-2012 out of pure curiosity
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄            tinkering with creating and configuring unofficial MMORPG emulators/servers.
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           Always trying to learn and improve things, evolve and surpass goals.
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓          Addicted to doing activities smartly, efficiently and quickly.
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       -----------
+▐▓                                 ▐▓        ABOUT
+▐▓           > D U U D             ▐▓        ${config.name} 
+▐▓                                 ▐▓        <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓       爵 <u><a href="${config.repo}" target="_blank">Github Repository</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓          -----------
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            CONTACT
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u> 
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
 
 `;
   }
